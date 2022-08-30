34f1201f1b13a71e27f14561a96f21185229e11b
youngentry <gentry_@naver.com>
2022-08-30 17:48:46 +0900

    220830

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-$(function () {
-  $("body").css("background", "#fff");
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.querySelector(".topRate strong").addEventListener("click", () => {
+        document.querySelector(".topRate").classList.toggle("on");
+    });
 });
